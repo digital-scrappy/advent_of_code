015aa4ab46f9d1f396f5f4e807df607522fd412a
Jakob Hauser <jakob.e.hauser@protonmail.com>
2020-12-01 14:37:15 +0100

    improved performance

--- 01.cpp
+++ 01.cpp
@@ -2,54 +2,36 @@
 #include <fstream>
 #include <iostream>
 #include <istream>
-#include <vector>
 using namespace std;
 
 
 
-// int main () {
-//   ifstream infile("01_input.txt");
-//   vector<int> arr;
-//   int indx = 0;
-//   int x;
-//   int d = 0;
-
-//   while (infile >> x) {
-//     arr.push_back(x);
-//   }
-//   for (int a = 0; a < arr.size(); a++) {
-//     for(int b = 0; b < arr.size(); b++) {
-//       for(int c = 0; c < arr.size(); c++) {
-//         d = arr[b]+arr[a]+arr[c];
-//       if (d == 2020) {
-//         cout << arr[a]*arr[b]*arr[c] << endl;
-//       }
-//       }
-//     }
-//   }
-//   return 0;
-// }
-
 int main () {
   ifstream infile("01_input.txt");
-  vector<int> arr;
+  int arr[200];
   int indx = 0;
   int x;
-  int d = 0;
-  int crazy = 0;
-  while (infile >> x) {
-    arr.push_back(x);
-  }
-  for (crazy; crazy < 1000001; crazy++){
-  for (int a = 0; a < arr.size(); a++) {
-    for(int b = 0; b < arr.size(); b++) {
-      d = arr[b]+arr[a];
-      if (d == 2020) {
-        cout << arr[a]*arr[b] << endl;
+  bool flag = false;
 
+  while (infile >> x) {
+    arr[indx] = x;
+    indx++;
+  }
+  for (int a = 0; a < 200; a++) {
+    if (flag) {break;}
+    for(int b = 0; b < 200; b++) {
+      if (flag) {break;}
+      for(int c = 0; c < 200; c++) {
+      if (arr[b]+arr[a]+arr[c] == 2020) {
+        cout << arr[a]*arr[b]*arr[c] << endl;
+        flag = true;
+        break;
+      }
       }
     }
+
   }
-  }
+
+
   return 0;
 }
